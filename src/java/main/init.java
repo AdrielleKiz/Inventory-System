675d41398b8aa65b9db92e2233feab250a553de4
AdrielleKiz <deguia.198314@cubao.sti.edu.ph>
2020-10-30 07:56:13 +0800

    Created file creation method for user_db

--- src/java/main/init.java
+++ src/java/main/init.java
@@ -1,12 +1,31 @@
 package src.java.main;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+
 import javax.swing.JFrame;
 
 public class init {
-    public void createFile(){
-        String currentDir = System.getProperty("user_id");
-        File filepath = new File(currentDir + "file.txt");
-        System.out.println("File Created");
+    public void createFile() {
+        String currentDir = System.getProperty("user.dir");
+        final String user_filepath = "D:\\Users\\Adrielle\\OneDrive - STI College Cubao\\Second year\\HCI\\Inventory System\\user.txt";
+        File filepath = new File(user_filepath);
+        Formatter user_db;
+
+        if (filepath.exists()) {
+
+            System.out.println("file exists");
+            System.out.println(filepath.getPath());
+        } else {
+            System.out.println("File isnt created");
+            System.out.print("Creating File...");
+            try {
+                user_db = new Formatter(user_filepath);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
     public boolean landingPage(boolean x) {
         JFrame frame = new JFrame();
--- src/java/main/login.java
+++ src/java/main/login.java
@@ -1,9 +1,9 @@
 package src.java.main;
 import javax.swing.JFrame;
 
-public class logIn {
+public class login {
     public boolean logInPage(boolean b){
-        JFrame frame = new Jframe();
+        JFrame frame = new JFrame();
         frame.setVisible(b);
         return b;
     }
