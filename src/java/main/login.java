98a705414f9d21830c947437d1a101696c7b092c
AdrielleKiz <deguia.198314@cubao.sti.edu.ph>
2020-10-30 18:07:12 +0800

    created encapsulators for login

--- src/java/main/login.java
+++ src/java/main/login.java
@@ -3,6 +3,9 @@ package src.java.main;
 import javax.swing.JFrame;
 
 public class login {
+    private String user_name;
+    private String user_pass;
+
     public void logInPage(){
         JFrame frame = new JFrame();
         frame.setTitle("Login Page");
@@ -10,5 +13,22 @@ public class login {
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setLocation(500, 150);
+    }
+    
+    private String getName(){
+        return getName();
+    }
+
+    private void setName(String username){
+        this.user_name = username;
+
+    }
+    private String getPass(){
+        return getPass();
+    }
+
+    private void setPass(String password){
+        this.user_pass = password;
+
     }
 }
