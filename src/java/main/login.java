ce32b49b93bd68f5a34562f3fd344cbcd9434e88
AdrielleKiz <deguia.198314@cubao.sti.edu.ph>
2020-10-31 09:09:09 +0800

    Added params to logInPage method

--- src/java/main/login.java
+++ src/java/main/login.java
@@ -1,18 +1,22 @@
 package src.java.main;
 
+import java.awt.LayoutManager;
+
 import javax.swing.JFrame;
+import javax.swing.border.Border;
 
 public class login {
     private String user_name;
     private String user_pass;
 
-    public void logInPage(){
+    public void logInPage(boolean b) {
         JFrame frame = new JFrame();
         frame.setTitle("Login Page");
         frame.setSize(600, 500);
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setLocation(500, 150);
+        frame.setVisible(b);
     }
     
     private String getName(){
