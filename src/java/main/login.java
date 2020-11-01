378af331a4c3bd4c7ec5b6cef7576ab5a28787af
AdrielleKiz <deguia.198314@cubao.sti.edu.ph>
2020-11-01 14:53:39 +0800

    rename variable frame to signInGui

--- src/java/main/login.java
+++ src/java/main/login.java
@@ -3,6 +3,7 @@ package src.java.main;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JsignInGui;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -18,19 +19,19 @@ public class login implements ActionListener {
 
     public void prepareGui() {
         //set constructors
-        JFrame frame = new JFrame();
+        JFrame signInGui = new JFrame();
         JTextField userfield = new JTextField("enter username", 20);
         JTextField userpass = new JTextField("enter pass", 20);
         JButton signButton = new JButton("Sign In");
 
-        //frame attr
-        frame.setTitle("Login Page");
-        frame.setSize(600, 500);
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.setLocation(500, 150);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //signInGui attr
+        signInGui.setTitle("Sign in Page");
+        signInGui.setSize(600, 500);
+        signInGui.setVisible(true);
+        signInGui.setResizable(false);
+        signInGui.setLocation(500, 150);
+        signInGui.setVisible(true);
+        signInGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //login panel attr
         JPanel pan1 = new JPanel(new BorderLayout());
         pan1.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
@@ -38,15 +39,10 @@ public class login implements ActionListener {
         pan1.add(userpass,BorderLayout.CENTER);
         pan1.add(signButton,BorderLayout.SOUTH);
         
-        frame.setContentPane(pan1);
-        frame.pack();
+        signInGui.setContentPane(pan1);
+        signInGui.pack();
 
         signButton.addActionListener(this);
-
-        /* frame.getContentPane().add(userfield, BorderLayout.NORTH);
-        frame.getContentPane().add(userpass, BorderLayout.CENTER);
-        frame.getContentPane().add(signButton, BorderLayout.SOUTH);
- */
                
     }
 
