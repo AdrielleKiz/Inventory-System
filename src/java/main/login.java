b843d2a64094012c6a329c9ed3c63c64bba27d4a
AdrielleKiz <deguia.198314@cubao.sti.edu.ph>
2020-11-01 12:34:17 +0800

    Added login panel attributes

--- src/java/main/login.java
+++ src/java/main/login.java
@@ -1,8 +1,10 @@
 package src.java.main;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.BorderLayout;
 
@@ -17,23 +19,31 @@ public class login {
         JTextField userpass = new JTextField("enter pass", 20);
         JButton signButton = new JButton("Sign In");
 
-        
+        //frame attr
         frame.setTitle("Login Page");
         frame.setSize(600, 500);
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setLocation(500, 150);
         frame.setVisible(b);
+        //login panel attr
+        JPanel pan1 = new JPanel(new BorderLayout());
+        pan1.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        pan1.add(userfield,BorderLayout.NORTH);
+        pan1.add(userpass,BorderLayout.CENTER);
+        pan1.add(signButton,BorderLayout.SOUTH);
 
-
-        frame.getContentPane().add(userfield, BorderLayout.NORTH);
-        frame.getContentPane().add(userpass, BorderLayout.CENTER);
-        frame.getContentPane().add(signButton, BorderLayout.SOUTH);
-
+        frame.setContentPane(pan1);
         frame.pack();
         
+        /* frame.getContentPane().add(userfield, BorderLayout.NORTH);
+        frame.getContentPane().add(userpass, BorderLayout.CENTER);
+        frame.getContentPane().add(signButton, BorderLayout.SOUTH);
+ */
+        
+        
     }
-    
+
     private String getName(){
         return getName();
     }
@@ -50,4 +60,8 @@ public class login {
         this.user_pass = password;
 
     }
+
+    
+        
+    
 }
