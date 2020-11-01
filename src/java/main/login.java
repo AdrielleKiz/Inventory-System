1c9d64c29bb3fd4ed2547db3d601df2bfcb9a86e
AdrielleKiz <deguia.198314@cubao.sti.edu.ph>
2020-11-01 11:29:04 +0800

    Added BorderLayout attributes to components

--- src/java/main/login.java
+++ src/java/main/login.java
@@ -4,8 +4,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.SpringLayout;
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 
 public class login {
     private String user_name;
@@ -14,13 +13,11 @@ public class login {
     public void prepareGui(boolean b) {
         //set constructors
         JFrame frame = new JFrame();
-        JTextField userfield = new JTextField(10);
-        JTextField userpass = new JTextField(10);
+        JTextField userfield = new JTextField("enter username", 20);
+        JTextField userpass = new JTextField("enter pass", 20);
         JButton signButton = new JButton("Sign In");
-        FlowLayout layout = new FlowLayout(FlowLayout.CENTER);
-        
 
-        frame.setLayout(layout);
+        
         frame.setTitle("Login Page");
         frame.setSize(600, 500);
         frame.setVisible(true);
@@ -29,9 +26,12 @@ public class login {
         frame.setVisible(b);
 
 
-        frame.getContentPane().add(userfield);
-        frame.getContentPane().add(userpass);
-        frame.getContentPane().add(signButton);
+        frame.getContentPane().add(userfield, BorderLayout.NORTH);
+        frame.getContentPane().add(userpass, BorderLayout.CENTER);
+        frame.getContentPane().add(signButton, BorderLayout.SOUTH);
+
+        frame.pack();
+        
     }
     
     private String getName(){
