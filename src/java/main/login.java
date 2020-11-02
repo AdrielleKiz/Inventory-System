9637424f37d1d2c9b5cdca1182a2edfb119fb26d
AdrielleKiz <deguia.198314@cubao.sti.edu.ph>
2020-11-03 00:59:23 +0800

    Sign in button now working

--- src/java/main/login.java
+++ src/java/main/login.java
@@ -1,20 +1,27 @@
 package src.java.main;
 
 import javax.swing.*;
+
+import sun.awt.www.content.audio.basic;
+
 import java.awt.BorderLayout;
 import java.awt.event.*;
-import java.awt.*;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.io.*;
 
-public class login{
+public class login extends init implements ActionListener {
+    public String user_name;
+    public String user_pass;
 
     JTextField userfield = new JTextField(20);
     JTextField userpass = new JTextField(20);
     JButton signButton = new JButton("Sign In");
 
     public void prepareGui() {
-        //set constructors
+        // set constructors
         JFrame signInGui = new JFrame();
-        //signInGui attr
+        // signInGui attr
         signInGui.setTitle("Sign in Page");
         signInGui.setSize(600, 500);
         signInGui.setVisible(true);
@@ -23,8 +30,7 @@ public class login{
         signInGui.setVisible(true);
         signInGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
-        //login panel attr
+        // login panel attr
         JPanel leftpan = new JPanel(new BorderLayout());
         leftpan.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         JPanel rightpan = new JPanel(new BorderLayout());
@@ -40,42 +46,83 @@ public class login{
         rightpan.add(userfield, BorderLayout.NORTH);
         rightpan.add(userpass, BorderLayout.CENTER);
         rightpan.add(signButton, BorderLayout.SOUTH);
-        
+
         leftpan.add(userNameLabel, BorderLayout.NORTH);
         leftpan.add(userPassLabel, BorderLayout.CENTER);
         leftpan.add(blank, BorderLayout.SOUTH);
 
         signButton.setActionCommand("Sign In");
-        signButton.addActionListener(new signin());
-        
+        signButton.addActionListener(this);
+
         signInGui.setContentPane(outerpan);
         signInGui.pack();
-        
-               
     }
-        //signin
-public class signin implements ActionListener{
-    private String user_name;
-    private String user_pass;
+    public void setUserNametf(ActionEvent b){
+        String command = b.getActionCommand();
+        if (command.equals("Sign In")) {
+        this.user_name = userfield.getText();
+        }
+    }
+    //set userpass
+    public void setUserPasstf(ActionEvent e){      
+        String command = e.getActionCommand();
+        if (command.equals("Sign In")) {
+        this.user_pass = userpass.getText();
+        }
+    }
+/* 
+    public String getUserNametf(String user_name){
+        return user_name;
+    }
+    public String getUserPasstf(String user_pass){
+        return user_pass;
+    } */
 
+    // Sign In Verification
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
         String command = e.getActionCommand();
+        if (command.equals("Sign In")) {
+        try {
+            Scanner sc;
+            int counter = 0;         
+                sc = new Scanner(filepath);
+                while(sc.hasNext() && counter != 2 && sc.hasNextLine()){
+                    sc.useDelimiter("[,]");
+                    String line = sc.nextLine();
+                    Scanner linecredentials = new Scanner(line);
+                    String user = linecredentials.next();
+                    String pass = linecredentials.next();
+                    if(user.equals(user_name)){
+                        counter++;
+                        System.out.println("user: "+ user + " user_name: " + user_name);
+                        if(pass.equals(user_pass)){
+                        counter++;
+                        System.out.println("pass: " + pass + "" + " userpass: "+ user_pass);
+                        System.out.println("Proper Credentials");
+                        linecredentials.close();
+                        sc.close();
+                        }else{
+                            System.out.println("Wrong Credentials");
+                        }
+                    }
 
-        if(command.equals("Sign In")){
-            this.user_name = userfield.getText();
-            this.user_pass = userpass.getText();
-            System.out.println(this.user_name);
-            System.out.println(this.user_pass);
+                    
+                }
+            } catch (FileNotFoundException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
 
         }
-        
-
     }
     
-
-}
-
+/*     public String getUserName(){
+        return user_name;
+    }
+    public String getUserPass(){
+        return user_pass;
+    } */
 
 
 
--- src/java/main/main.java
+++ src/java/main/main.java
@@ -1,19 +1,15 @@
 package src.java.main;
-public class main {
+
+public class main{
     public static void main(String[] args) {
         init p = new init();
         login a = new login();
-
-        a.prepareGui();
         p.initFile();
-
+        a.prepareGui();
 
         
 
 
-
-
-
-
-    }    
+        
+    }
 }
